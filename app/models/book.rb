5330b9ef4360c45d17972880ed5276efef7ff623
root <root@localhost>
2022-09-15 01:34:51 +0000

    made unit and integration tests, and updated book attributes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
     validates :title, presence: true
+    t.string :author
+    t.integer :price
+    t.date :published-date
 end
--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,11 +2,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
+  scenario 'valid title' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+  end
+
+  scenario 'valid author' do
+    visit new_book_path
+    fill_in 'Author' with: 'J.K. Rowling' 
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('J.K. Rowling')
+  end
+
+  scenario 'valid price' do
+    visit new_book_path
+    fill_in 'Price' with: 30 
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('30')
+  end
+  
+  scenario 'valid published date' do
+    visit new_book_path
+    fill_in 'Published Date' with: '12/10/2001' 
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('12/10/2001')
   end
 end
